1fdabf965bd13a40a7c25735e5448d15cc890159
Amit Biswas <amit.biswas6991@gmail.com>
2020-10-23 21:12:10 +0600

    updated requirements

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("register", RegistrationView.as_view()),
     path("login", CustomTokenObtainPairView.as_view()),
     path("refresh-token", TokenRefreshView.as_view()),
-    # path("user", UserView.as_view()),
+    path("user", UserView.as_view()),
     path("profile", ProfileView.as_view()),
     path("apply-for-license", LicenseUpdateView.as_view()),
 ]
