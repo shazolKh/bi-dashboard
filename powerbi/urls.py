b1d88979a87e8459e065993b28553a743b5ec161
Amit Biswas <amit.biswas6991@gmail.com>
2020-10-30 14:31:24 +0600

    updated openapi documentaion

--- powerbi/urls.py
+++ powerbi/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from .views import DashboardListView, DashboardView
 
@@ -9,5 +8,3 @@ urlpatterns = [
     path("", DashboardListView.as_view(), name="dashboard_list"),
     path("<str:id>", DashboardView.as_view(), name="dashboard"),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
